e8d2f464d8532d3d5899eec00fa2160b27f1727a
ab-bash <65164933+ab-bash@users.noreply.github.com>
2022-06-21 16:40:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,20 @@
-provider "aws" {
+#provider "aws" {
 
-  region                  = var.region
+#  region                  = var.region
 #  shared_credentials_file = var.shared_credentials_file
+#}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "aws" {
+	region	=	var.region
+# Configuration options
 }
