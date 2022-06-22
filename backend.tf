2b78be149554c2dad3f22b78cabd42d93a3db270
ab-bash <aabidbashar@gmail.com>
2022-06-22 10:58:13 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-state-bucket-word"
     key            = "states/terraform.tfstate"
     region 	   = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+#    dynamodb_table = "terraform-lock"
   }
 }
